8cad1e88dea77b2bbecad4eac7a8339135284315
Marta Scardillo <martascardillo02@gmail.com>
2023-11-23 16:21:13 +0100

    Add include functions.php

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
-
-
-
-
-function passwordGenerator($password_length)
-{
-    // creo una stringa con numeri e lettere tra cui scegliere
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_';
-
-    // conto i caratteri nella stringa per poi creare un limite per il numero random
-    $charactersLength = strlen($characters);
-
-    // creo la stringa in cui verrà messa la password
-    $password = '';
-
-    // creo con il ciclo for la password utilizzando la lunghezza di charaters come limite massimo del generatore random
-    for ($i = 0; $i < $password_length; $i++) {
-        $password .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $password;
-
-}
-
-
+include("./partials/functions.php");
 ?>
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
